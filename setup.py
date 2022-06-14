62665aa3b04da3f37d3864b9dfd7378fd2e6bd8e
gpetter <graysonpetter@gmail.com>
2022-06-14 13:08:14 -0400

    setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='halomodelpy', #project name
+    version='0.1.0',
+    description='calculate HM observables',
+    #url
+    author='Petter',
+    author_email='Grayson.C.Petter.GR@dartmouth.edu', #not a real e-mail
+    license='',
+    packages=find_packages(),
+    install_requires=[]
+)
